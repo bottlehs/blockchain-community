b8d041b5b747307a318be0c67351cf515baad060
bottlehs <qudgns9@gmail.com>
2021-01-20 13:31:49 +0900

    dapp model update

--- backend/app/models/dapps.model.js
+++ backend/app/models/dapps.model.js
@@ -4,18 +4,46 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.BIGINT(20),
       allowNull: false,
     },
+    logo: {
+      type: Sequelize.STRING(100),
+      allowNull: false,
+    },    
     name: {
       type: Sequelize.STRING(100),
       allowNull: false,
     },
-    description: {
+    website: {
+      type: Sequelize.STRING(100),
+      allowNull: false,
+    },    
+    previewImage: {
       type: Sequelize.STRING,
+      allowNull: false,
+    },    
+    category: {
+      type: Sequelize.STRING(50),
+      allowNull: false,
+    },    
+    tags: {
+      type: Sequelize.STRING(100),
       allowNull: false,
     },
     status: {
       type: Sequelize.STRING(20),
       allowNull: false,
       defaultValue: "publish",
+    },
+    shortDescription: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    detailDescription: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    reviewArticle: {
+      type: Sequelize.STRING(100),
+      allowNull: false,
     },
     ipAddress: {
       type: Sequelize.STRING(100),
