e652d923748dbca0691f519a6e92dbaad89577f3
bottlehs <qudgns9@gmail.com>
2021-01-20 17:50:12 +0900

    dapps model 수정

--- backend/app/models/dapps.model.js
+++ backend/app/models/dapps.model.js
@@ -8,6 +8,10 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.BIGINT(20),
       allowNull: false,
     },
+    categoriesId: {
+      type: Sequelize.BIGINT(20),
+      allowNull: false,
+    },    
     isOwner: {
       type: Sequelize.BOOLEAN,
       allowNull: false,
@@ -52,18 +56,76 @@ module.exports = (sequelize, Sequelize) => {
       allowNull: false,
       defaultValue: "live",
     },    
-    abstract: { // shortDescription
+    abstract: { 
       type: Sequelize.TEXT,
       allowNull: false,
     },
-    description: { // detailDescription
+    description: { 
       type: Sequelize.TEXT,
       allowNull: false,
     },
-    articleLink: { // reviewArticle
+    articleLink: { 
+      type: Sequelize.STRING(100),
+      allowNull: false,
+    },  
+    // Affiliate/Referral Program
+    hasAffiliateLink: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: 0,
+    },        
+    affiliateLink: {
       type: Sequelize.STRING(100),
       allowNull: false,
     },
+    // Smart Contracts Info ( 가 2개일 경우 요청을 2번 보냄 )
+    onChain: {
+      type: Sequelize.TINYINT(1),
+      allowNull: false,
+    },    
+    chainsIds: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },      
+    address: {
+      type: Sequelize.TEXT,
+      allowNull: false,      
+    },    
+    // Token Info ( 별도 테이블로 분리 할것 )
+    hasToken: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: 0,
+    },    
+    tokenChainsId: {
+      type: Sequelize.BIGINT(20),
+      allowNull: false,
+    },
+    tokenCoingeckoLink: { 
+      type: Sequelize.STRING(100),      
+      allowNull: false,
+    },
+    tokenContract: { 
+      type: Sequelize.STRING(100),      
+      allowNull: false,
+    },      
+    tokenLogo: {
+      type: Sequelize.STRING(100),      
+      allowNull: false,
+    },
+    tokenName: { 
+      type: Sequelize.STRING(100),      
+      allowNull: false,
+    },
+    tokenTicker: { 
+      type: Sequelize.STRING(100),      
+      allowNull: false,
+    },
+    decimal: {
+      type: Sequelize.BIGINT(20),
+      allowNull: false,
+    },
+    // End Token Info
     ipAddress: {
       type: Sequelize.STRING(100),
       allowNull: false,
