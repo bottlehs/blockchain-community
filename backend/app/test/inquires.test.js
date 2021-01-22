1c6f80eafd0864ed17639fbe558ec17b066b7faa
bottlehs <qudgns9@gmail.com>
2021-01-22 16:34:35 +0900

    init jest inquires.routes

---
+++ backend/app/test/inquires.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const app = require('../../server.jest');
+require("../routes/inquires.routes")(app);
+
+jest.mock("./save_json", () => ({
+  save: jest.fn(),
+}));
+
+jest.mock("./usStates.json", () => [
+  {
+    state: "MI",
+    capital: "Lansing",
+    governor: "Gretchen Whitmer",
+  },
+  {
+    state: "GA",
+    capital: "Atlanta",
+    governor: "Brian Kemp",
+  },
+]);
+
+describe("testing-server-inquires-routes", () => {
+  it("GET /api/inquires - success", async () => {
+    const response = await request(app).get("/api/inquires");
+    expect(response.statusCode).toEqual(200);
+  });
+});
