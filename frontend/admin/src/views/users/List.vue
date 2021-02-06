17f1287acb9fc66b07eed16c22b916ae041b84b0
bottlehs <qudgns9@gmail.com>
2021-02-06 13:40:23 +0900

    회원 검색

--- frontend/admin/src/views/users/List.vue
+++ frontend/admin/src/views/users/List.vue
@@ -2,8 +2,20 @@
   <div class="list">
     <b-container fluid>
       <!-- 검색 폼 -->
-      <b-row> </b-row>
-
+      <b-form inline @submit="onSubmit" @reset="onReset">
+        <b-form-select
+          class="mb-2 mr-sm-2 mb-sm-0"
+          v-model="search.type"
+          :options="searchTypeOptions"
+          :value="null"
+        ></b-form-select>
+        <b-form-input
+          class="mb-2 mr-sm-2 mb-sm-0"
+          v-model="search.q"
+          placeholder="검색"
+        ></b-form-input>
+        <b-button variant="primary" type="submit">검색</b-button>
+      </b-form>
       <!-- 검색 결과 -->
       <b-table
         striped
@@ -82,6 +94,7 @@ export default {
     return {
       /**
        * search : 검색 데이터
+       * searchTypeOptions : 검색 항목
        * fields : 검색결과 페이지 리스트 필드
        * items : 응답 리스트 데이터
        * page : 검색결과 페이지 데이터
@@ -100,13 +113,15 @@ export default {
         type: "",
         q: ""
       },
+      searchTypeOptions: [],
       fields: [
         {
           /**
            * 이메일
            */
           key: "email",
-          label: this.$t("users_email")
+          label: this.$t("users_email"),
+          isSearch: true
         },
         {
           /**
@@ -202,6 +217,25 @@ export default {
     /**
      * mounted
      */
+    let type = '';
+    this.fields.forEach(row => {
+      if ( row.isSearch ) {
+        this.searchTypeOptions.push({
+          text: row.label,
+          value: row.key
+        });
+
+        if ( this.search.type == row.key ) {
+          type = row.key;
+        };
+      }
+    });
+
+    if ( type ) {
+      this.search.type = type
+    } else {
+      this.search.type = this.searchTypeOptions[0].value;
+    }
   },
   computed: {
     /**
@@ -220,10 +254,13 @@ export default {
      */
     async onSubmit(evt) {
       evt.preventDefault();
+
+      this.findAll();
     },
     onReset(evt) {
       evt.preventDefault();
     },
+
     findAll() {
       this.wait = true;
 
